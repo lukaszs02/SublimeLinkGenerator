0fdde4b653b45d65fee62c83b4dec9bc45b3097c
Lukasz <lukaszstepien02@gmail.com>
2018-05-01 14:52:11 +0200

    Add link generation and copy to clipboar (OSX only)

--- LinkGeneratorMain.py
+++ LinkGeneratorMain.py
@@ -1,13 +1,23 @@
+from os.path import dirname, realpath
+import os
+import re
 import sublime
 import sublime_plugin
-
 
 class GeneratelinkCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         settings = sublime.load_settings('LinkGenerator.sublime-settings')
-        link = settings.get("url") + settings.get("lineSeparator") + str(self.current_line())
+        link = settings.get("url") + self.file_path() + settings.get("lineNumberSeparator") + str(self.current_line())
         print(link)
-
+        self.copy_link_to_clipboard(link)
 
     def current_line(self):
         return (self.view.rowcol(self.view.sel()[0].begin())[0]) + 1
+
+    def file_path(self):
+        project_folder = re.search("[^/]*$", str(self.view.window().extract_variables()['folder'])).group(0)
+        file_patch = re.search(project_folder + "/.*[^/]*$", str(self.view.window().extract_variables()['file'])).group(0)
+        return file_patch
+
+    def copy_link_to_clipboard(self, txt):
+        os.system("echo '%s' | pbcopy" % txt)
