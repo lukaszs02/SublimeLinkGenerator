d829c47f550b752ae0e63897eed3d0ec323b738b
Lukasz <lukaszstepien02@gmail.com>
2018-05-01 13:13:02 +0200

    Add settings file and part of link genration

--- LinkGeneratorMain.py
+++ LinkGeneratorMain.py
@@ -4,6 +4,10 @@ import sublime_plugin
 
 class GeneratelinkCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        print("GenerateLink")
-        print(self.view.window().project_file_name())
+        settings = sublime.load_settings('LinkGenerator.sublime-settings')
+        link = settings.get("url") + settings.get("lineSeparator") + str(self.current_line())
+        print(link)
 
+
+    def current_line(self):
+        return (self.view.rowcol(self.view.sel()[0].begin())[0]) + 1
