278287e634f14274d1d8aa0c94b61eb228e8b669
Lukasz Stepien <lukaszstepien02@gmail.com>
2018-05-08 18:29:51 +0200

    Added support for MS Windows

--- LinkGeneratorMain.py
+++ LinkGeneratorMain.py
@@ -3,13 +3,13 @@ import os
 import re
 import sublime
 import sublime_plugin
-
+ 
 class GeneratelinkCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         link = self.generateLink()
         print(link)
-        self.copyLinkToClipboard(link)
-
+        sublime.set_clipboard(link)
+ 
     def generateLink(self):
         settings = sublime.load_settings('LinkGenerator.sublime-settings')
         if(re.search("gitlab|github", settings.get("url"))):
@@ -17,24 +17,21 @@ class GeneratelinkCommand(sublime_plugin.TextCommand):
         else:
             link = settings.get("url") + self.projectName() + self.filePath() + settings.get("lineNumberSeparator") + str(self.currentLine())
         return link
-
+ 
     def currentLine(self):
         return (self.view.rowcol(self.view.sel()[0].begin())[0]) + 1
-
+ 
     def projectName(self):
-         projectName = re.search("[^/]*$", str(self.view.window().extract_variables()['folder'])).group(0)
+         projectName = re.search(r'[^(\\|/)]*$', str(self.view.window().extract_variables()['folder'])).group(0)
          return projectName
-
+ 
     def filePath(self):
-        project_folder = re.search("[^/]*$", str(self.view.window().extract_variables()['folder'])).group(0)
-        file_patch = re.search("(?:" + project_folder + ")(.*$)", str(self.view.window().extract_variables()['file'])).group(1)
+        project_folder = self.projectName()
+        file_patch = re.search(r'(?:' + project_folder + r')(.*$)', str(self.view.window().extract_variables()['file'])).group(1)
         return file_patch
-
+ 
     def getBranch(self, settings):
         branch = settings.get("branch", "master")
         if(branch == "_auto"):
             branch = "master" #TODO add geting current branch form repository
         return branch
-
-    def copyLinkToClipboard(self, txt):
-        os.system("echo '%s' | pbcopy" % txt) #TODO add support for windows
